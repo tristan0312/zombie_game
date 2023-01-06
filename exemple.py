9793429bdb2d2390964c886c1af88bfbec3d39fb
tristan0312 <99179072+tristan0312@users.noreply.github.com>
2023-01-06 21:05:33 +0100

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -2,6 +2,7 @@ import pygame, sys
 from pygame.locals import *
 import random
 import math
+import time
 pygame.init()
 pygame.key.set_repeat(50)
 
@@ -64,6 +65,10 @@ class Player:
     def barre_vie(self):
         couleur_barre_vie = (111, 210, 46)
         couleur_fond_barre = (0, 0, 0)
+        if self.vie <= 25 and self.vie > 10:
+            couleur_barre_vie = (252, 126, 0)
+        elif self.vie <= 10:
+            couleur_barre_vie = (255, 0, 0)
         
         barre_position = [self.rect[0] + 10, self.rect[1] - 10, self.vie, 6]
         barre_position_fond = [self.rect[0] + 10, self.rect[1] - 10, self.max_vie, 6]
@@ -111,8 +116,8 @@ class Player:
 pygame.init()
 clock = pygame.time.Clock()            
 
-screen_width = 1280
-screen_height = 720
+screen_width = 1800
+screen_height = 1200
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("zombie attack")
             
@@ -123,22 +128,37 @@ position_BG.topleft = (-1100, -1300)
 
 class Zombie:
     def __init__(self):
+        self.monstre_image = pygame.image.load('0.png')
         self.taille = 10
         self.x = random.randint(0,  screen_width - self.taille)
         self.y = random.randint(0,  screen_height - self.taille)
         self.vitesse = 1
-        self.couleur = (26, 162, 53)
+        self.vie = 50
+        self.max_vie = 50
+        self.deplacement = []
+        self.deplacement.append(pygame.image.load('0.png'))
+        self.deplacement.append(pygame.image.load('1.png'))
+        self.deplacement.append(pygame.image.load('2.png'))
+        self.deplacement.append(pygame.image.load('3.png'))
+        self.sprite_actuelle = 0
+        self.mouvement = False
+        
+    def mouvements(self):
+        self.mouvement = True
         
     def affichage(self):
-        pygame.draw.circle(screen, self.couleur, (self.x, self.y), self.taille)
+        screen.blit(self.monstre_image, (self.x, self.y))
         
     def mvt(self, target_x, target_y):
         distance_x = target_x - self.x
         distance_y = target_y - self.y
         distance = (distance_x**2 + distance_y**2)**0.5
         
-        if distance <  450:
-            if self.x <player.rect[0]:
+        if distance <= 40:
+            player.vie -= 0.5
+        
+        if distance <  600:
+            if self.x < player.rect[0]:
                 self.x += self.vitesse
             if self.x > player.rect[0]:
                 self.x -= self.vitesse
@@ -149,16 +169,63 @@ class Zombie:
         else:
             self.x = self.x
             self.y = self.y
+            
+    def barre_vie(self):
+        couleur_barre_vie = (255, 0, 0)
+        couleur_fond_barre = (0, 0, 0)
+        
+        
+        barre_position = [self.x + 10, self.y - 10, self.vie, 6]
+        barre_position_fond = [self.x + 10, self.y - 10, self.max_vie, 6]
+        
+        pygame.draw.rect(screen, couleur_fond_barre, barre_position_fond)
+        pygame.draw.rect(screen, couleur_barre_vie, barre_position)
+
+            
+    def update(self, speed):
+        if self.mouvement == True:
+            self.sprite_actuelle += speed
+            if int(self.sprite_actuelle) >= len(self.deplacement):
+                self.sprite_actuelle = 0
+                self.mouvement = False
+            self.monstre_image = self.deplacement[int(self.sprite_actuelle)]
+  
+class Playerbullet:
+    def __init__(self, x, y, mouse_x, mouse_y):
+        self.x = x
+        self.y = y
+        self.mouse_x = mouse_x
+        self.mouse_y = mouse_y
+        self.speed = 5
+        self.angle = math.atan2(mouse_y - self.y, mouse_x - self.x)
+        self.x_vel = math.cos(self.angle) * self.speed
+        self.y_vel = math.sin(self.angle) * self.speed
+    def main(self, screen):
+        self.x += self.x_vel
+        self.y += self.y_vel
+        pygame.draw.circle(screen, (0,0,0),(self.x, self.y), 5)
+        
 
 
-    
-            
-            
+def loose():
+    police = pygame.font.SysFont('John Hubbard',120)
+    image_texte = police.render("game over", 1, (255,0,0))
+    screen.blit(image_texte,(700 ,500))
+    pygame.display.flip()
+
+
 lst_zmb = [Zombie() for k in range(5)]
+player_bullets = []
 def main_jeux():   
     while True:
         screen.fill('blue')
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        
         for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    player_bullets.append(Playerbullet(player.rect[0], player.rect[1] + 10, mouse_x, mouse_y))
+                    
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
                     player.mouvement_droite()
@@ -185,20 +252,49 @@ def main_jeux():
                 sys.exit()
                 pygame.quit()
                 
-      
-            
+           
+                
+
         screen.blit(BG, position_BG)
+        
+        if player.vie <= 0:
+            loose()
+            time.sleep(3)
+            sys.exit()
+            pygame.quit()
+             
         for zmb in lst_zmb:
             zmb.affichage()
+            zmb.barre_vie()
+            zmb.mouvements()
             zmb.mvt(player.rect[0], player.rect[1])
+            zmb.update(0.25)
+            
+        for bullet in player_bullets:
+            bullet.main(screen)
+           
+           
+        for bullet in player_bullets:
+            for zmb in lst_zmb:
+                if zmb.vie <= 0:
+                    zmb.x = -100
+                    zmb.y = -100
+                   
+                    
+                if zmb.x - 30  <bullet.x< zmb.x + 30 and zmb.y - 30  <bullet.y< zmb.y + 30:
+                    zmb.vie -= 1
+            
         player.barre_vie()
         player.draw()
         player.update(0.25)
+        
         pygame.display.flip()
         clock.tick(60)
                 
                 
-main_jeux()       
+main_jeux()
+
+
     
         
         
