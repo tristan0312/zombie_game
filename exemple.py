6a4b5077f2cd12b28eab9fd23105698dae84a69d
tristan0312 <99179072+tristan0312@users.noreply.github.com>
2023-01-22 15:03:35 +0100

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 import random
+from random import randint
 import math
 import time
 pygame.init()
@@ -119,19 +120,23 @@ clock = pygame.time.Clock()
 screen_width = 1800
 screen_height = 1200
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("zombie attack")
+pygame.display.set_caption("monster attack")
             
 player = Player(screen_width/2, screen_height/2)
 BG = pygame.image.load('bg.png').convert_alpha()
 position_BG = BG.get_rect()
 position_BG.topleft = (-1100, -1300)
 
-class Zombie:
+
+class Zombie(pygame.sprite.Sprite):
     def __init__(self):
-        self.monstre_image = pygame.image.load('0.png')
+        super().__init__()
+        self.image = pygame.image.load('0.png')
+        self.rect = self.image.get_rect()
         self.taille = 10
-        self.x = random.randint(0,  screen_width - self.taille)
-        self.y = random.randint(0,  screen_height - self.taille)
+        self.x = randint(100,500)
+        self.y = randint(100,500)
+        self.rect.topleft = [self.x,self.y]
         self.vitesse = 1
         self.vie = 50
         self.max_vie = 50
@@ -143,52 +148,62 @@ class Zombie:
         self.sprite_actuelle = 0
         self.mouvement = False
         
+        
     def mouvements(self):
         self.mouvement = True
-        
-    def affichage(self):
-        screen.blit(self.monstre_image, (self.x, self.y))
-        
-    def mvt(self, target_x, target_y):
-        distance_x = target_x - self.x
-        distance_y = target_y - self.y
-        distance = (distance_x**2 + distance_y**2)**0.5
-        
-        if distance <= 40:
-            player.vie -= 0.5
-        
-        if distance <  600:
-            if self.x < player.rect[0]:
-                self.x += self.vitesse
-            if self.x > player.rect[0]:
-                self.x -= self.vitesse
-            if self.y < player.rect[1]:
-                self.y += self.vitesse
-            if self.y > player.rect[1]:
-                self.y -= self.vitesse
-        else:
-            self.x = self.x
-            self.y = self.y
-            
-    def barre_vie(self):
-        couleur_barre_vie = (255, 0, 0)
-        couleur_fond_barre = (0, 0, 0)
-        
-        
-        barre_position = [self.x + 10, self.y - 10, self.vie, 6]
-        barre_position_fond = [self.x + 10, self.y - 10, self.max_vie, 6]
-        
-        pygame.draw.rect(screen, couleur_fond_barre, barre_position_fond)
-        pygame.draw.rect(screen, couleur_barre_vie, barre_position)
-
-            
+     
     def update(self, speed):
         if self.mouvement == True:
             self.sprite_actuelle += speed
             if int(self.sprite_actuelle) >= len(self.deplacement):
                 self.sprite_actuelle = 0
                 self.mouvement = False
-            self.monstre_image = self.deplacement[int(self.sprite_actuelle)]
+            self.image = self.deplacement[int(self.sprite_actuelle)]
+            
+        
+    def distance_joueur(self, target_x, target_y):
+        distance_x = target_x  - self.rect[0] 
+        distance_y = target_y  - self.rect[1] 
+        distance = (distance_x**2 + distance_y**2)**0.5
+        
+        if distance <= 40:
+            player.vie -= 0.5
+        
+        if distance <  700:
+            if self.rect[0] < player.rect[0]:
+                self.rect[0] += self.vitesse
+            if self.rect[0] > player.rect[0]:
+                self.rect[0] -= self.vitesse
+            if self.rect[1] < player.rect[1]:
+                self.rect[1] += self.vitesse
+            if self.rect[1] > player.rect[1]:
+                self.rect[1] -= self.vitesse
+                
+    def barre_vie(self):
+        couleur_barre_vie = (255, 0, 0)
+        couleur_fond_barre = (0, 0, 0)
+        
+        
+        barre_position = [self.rect[0] + 10, self.rect[1] - 10, self.vie, 6]
+        barre_position_fond = [self.rect[0] + 10, self.rect[1] - 10, self.max_vie, 6]
+        
+        pygame.draw.rect(screen, couleur_fond_barre, barre_position_fond)
+        pygame.draw.rect(screen, couleur_barre_vie, barre_position)
+        
+        
+    def distance_balle(self, target_x, target_y):
+        distance_x = target_x - self.rect[0]
+        distance_y = target_y - self.rect[1]
+        distance = (distance_x**2 + distance_y**2)**0.5
+        
+        if distance <= 60:
+            self.vie -= 0.8
+                
+   
+                
+    
+            
+            
   
 class Playerbullet:
     def __init__(self, x, y, mouse_x, mouse_y):
@@ -196,27 +211,41 @@ class Playerbullet:
         self.y = y
         self.mouse_x = mouse_x
         self.mouse_y = mouse_y
-        self.speed = 5
+        self.speed = 4
         self.angle = math.atan2(mouse_y - self.y, mouse_x - self.x)
         self.x_vel = math.cos(self.angle) * self.speed
         self.y_vel = math.sin(self.angle) * self.speed
+        self.taille = 5
+    
     def main(self, screen):
         self.x += self.x_vel
         self.y += self.y_vel
-        pygame.draw.circle(screen, (0,0,0),(self.x, self.y), 5)
+        pygame.draw.circle(screen, (0,0,0),(self.x, self.y), self.taille)
+        if self.x > screen_width or self.x < 0:
+            self.taille = 0
+        
+        if self.y > screen_height or self.y < 0:
+            self.taille = 0
+        
+        
         
 
 
 def loose():
     police = pygame.font.SysFont('John Hubbard',120)
-    image_texte = police.render("game over", 1, (255,0,0))
+    image_texte = police.render("Game Over", 1, (255,0,50))
     screen.blit(image_texte,(700 ,500))
     pygame.display.flip()
 
 
-lst_zmb = [Zombie() for k in range(5)]
+
 player_bullets = []
-def main_jeux():   
+zombie = pygame.sprite.Group()
+for i in range(5):
+    monstre = Zombie()
+    zombie.add(monstre)
+
+def main_jeux():
     while True:
         screen.fill('blue')
         mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -226,36 +255,59 @@ def main_jeux():
                 if event.button == 1:
                     player_bullets.append(Playerbullet(player.rect[0], player.rect[1] + 10, mouse_x, mouse_y))
                     
+                    
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
                     player.mouvement_droite()
                     position_BG[0] -= 25
-                    for zmb in lst_zmb:
-                        zmb.x -= 25
+                    for bullet in  player_bullets:
+                        bullet.x -= 25
+                    for esprit in zombie:
+                        esprit.rect[0] -= 25
                 if event.key == pygame.K_z:
                     position_BG[1] += 25
-                    for zmb in lst_zmb:
-                        zmb.y += 25
+                    for bullet in  player_bullets:
+                        bullet.y += 25
+                    for esprit in zombie:
+                        esprit.rect[1] += 25
                     player.mouvement_haut()
                 if event.key == pygame.K_s:
+                    for bullet in  player_bullets:
+                        bullet.y -= 25
+                    for esprit in zombie:
+                        esprit.rect[1] -= 25
                     position_BG[1] -= 25
-                    for zmb in lst_zmb:
-                        zmb.y -= 25
                     player.mouvement_bas()
                 if event.key == pygame.K_q:
                     player.mouvement_gauche()
                     position_BG[0] += 25
-                    for zmb in lst_zmb:
-                        zmb.x += 25
+                    for bullet in  player_bullets:
+                        bullet.x += 25
+                    for esprit in zombie:
+                        esprit.rect[0] += 25
                   
             if event.type == pygame.QUIT:
                 sys.exit()
                 pygame.quit()
-                
-           
-                
+
 
         screen.blit(BG, position_BG)
+        zombie.draw(screen)
+        zombie.update(0.25)
+        for esprit in zombie:
+            esprit.mouvements()
+            esprit.distance_joueur(player.rect[0], player.rect[1])
+            esprit.barre_vie()
+            if esprit.vie <=0:
+               esprit.kill()
+            for bullet in player_bullets:
+                esprit.distance_balle(bullet.x, bullet.y)
+            
+            
+            
+        for bullet in player_bullets:
+            bullet.main(screen)
+        
         
         if player.vie <= 0:
             loose()
@@ -263,24 +315,10 @@ def main_jeux():
             sys.exit()
             pygame.quit()
              
-        for zmb in lst_zmb:
-            zmb.affichage()
-            zmb.barre_vie()
-            zmb.mouvements()
-            zmb.mvt(player.rect[0], player.rect[1])
-            zmb.update(0.25)
+       
             
-        for bullet in player_bullets:
-            bullet.main(screen)
-           
-           
-        for bullet in player_bullets:
-            for zmb in lst_zmb:
-                if zmb.vie <= 0:
-                    zmb.x = -100
-                    zmb.y = -100 
-                if zmb.x - 40  <bullet.x< zmb.x + 40 and zmb.y - 40  <bullet.y< zmb.y + 40:
-                    zmb.vie -= 1
+            
+          
             
         player.barre_vie()
         player.draw()
@@ -291,23 +329,5 @@ def main_jeux():
                 
                 
 main_jeux()
-
-
-
-
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-
-
 
 
